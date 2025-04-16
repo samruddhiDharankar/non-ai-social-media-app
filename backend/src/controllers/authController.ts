ff404e780b4ae661d9e38c0564327792a1510451
Samruddhi Dharankar <samruddhee.dharankar@gmail.com>
2025-04-16 15:15:55 -0700

    Update authController.ts
    added signup function

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -22,3 +22,20 @@ export const loginUserByEmailAndPassword = async (
     res.status(401).json({ message: "Invalid credentials" });
   }
 };
+
+export const signupUser = async (req: Request, res: Response) => {
+  const {username, name, email, password} = req.body;
+  const user - await User.findOne({ email });
+  if(user) {
+    res.status(404).json({ message: "Email is already used" });
+  }
+
+  const createdUser = await User.create({
+    username: username,
+    name: name,
+    email: email,
+    hashedPassword: password
+  });
+
+  console.log("user created", createdUser);
+};
