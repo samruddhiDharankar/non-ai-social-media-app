dc55e90661b1291a28d3934880756daa6878803d
Samruddhi Dharankar <samruddhee.dharankar@gmail.com>
2025-04-16 12:56:59 -0700

    Update User.ts
    added more fields to the user schema and validations

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -4,13 +4,29 @@ export interface IUser extends Document {
   name: string;
   email: string;
   hashedPassword: string;
+  username: string;
+  bio?: string;
+  profilePictUrl?: string;
+  tier: string;
+  averageAuthScore: number;
+  postCount: number;
+  streakCount: number;
+  lastLogin?: Date 
 }
 
 const userSchema = new Schema<IUser>({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   hashedPassword: { type: String, required: true },
-});
+  username: { type: String, required: true },
+  bio: { type: String },
+  profilePictUrl: { type: String },
+  tier: { type: String, default: 'Rookie' },
+  averageAuthScore: { type: Number, default: 0 },
+  postCount: { type: Number, default: 0 },
+  streakCount: { type: Number, default: 0 },
+  lastLogin: { typs: Date, default: null },
+}, { timestamps: true });
 
 const User = mongoose.model<IUser>("User", userSchema);
 
