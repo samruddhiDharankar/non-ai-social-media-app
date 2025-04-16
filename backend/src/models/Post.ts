110c2218bcf7baa541f2c7c4e973e90eebfa1324
Samruddhi Dharankar <samruddhee.dharankar@gmail.com>
2025-04-16 13:22:40 -0700

    Create Post.ts
    created Post.ts model and added userId foreign key relation

---
+++ backend/src/models/Post.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IPost extends Document {
+  text: string;
+  imageUrl?: string;
+  authScore: number;
+  aiDetectionSummary: string;
+  tierAtPostTime: string;
+  userId: Types.ObjectId;  // refers to User document
+}
+
+const postSchema = new Schema<IPost>({
+  text: { type: String, default: "" },
+  imageUrl: { type: String },
+  authScore: { type: Number, default: 0 },
+  aiDetectionSummary: { type: String, default: "" },
+  tierAtPostTime: { type: String, default: "" },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User", //reference to the User model
+    required: true
+  }
+}, { timestamps: true });
+
+const Post = mongoose.model<IPost>("Post", postSchema);
+
+export default Post;
