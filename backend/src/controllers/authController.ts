0fe384530f2c8d554ba382a9a82f3e7f77f943ac
samruddhiDharankar <samruddhee.dharankar@gmail.com>
2025-04-16 19:50:57 -0700

    added hashing to passwords using bcrypt

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,20 +1,21 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const loginUserByEmailAndPassword = async (
   req: Request,
   res: Response
 ) => {
   const { email, password } = req.body;
-
-  const user = await User.findOne({ email });
+  const normalizedEmail = email.toLowerCase().trim();
+  const user = await User.findOne({ email: normalizedEmail });
   if (!user) {
     res.status(404).json({ message: "User not found" });
     return;
   }
-
-  if (password === user?.hashedPassword) {
+  const isPasswordCorrect = await bcrypt.compare(password, user.hashedPassword);
+  if (isPasswordCorrect) {
     user.lastLogin = new Date();
     await user.save();
     const token = jwt.sign({ id: user?._id }, process.env.JWT_SECRET!, {
@@ -29,17 +30,21 @@ export const loginUserByEmailAndPassword = async (
 
 export const signupUser = async (req: Request, res: Response) => {
   const { username, name, email, password } = req.body;
-  const user = await User.findOne({ email });
+  const normalizedEmail = email.toLowerCase().trim();
+  const user = await User.findOne({ email: normalizedEmail });
   if (user) {
-    res.status(404).json({ message: "Email is already used" });
+    res.status(409).json({ message: "Email is already used" });
     return;
   }
-
+  const hashedPassword = await bcrypt.hash(
+    password,
+    Number(process.env.BCRYPT_SALT_ROUNDS)
+  );
   const createdUser = await User.create({
     username: username,
     name: name,
-    email: email,
-    hashedPassword: password,
+    email: normalizedEmail,
+    hashedPassword: hashedPassword as string,
   });
   res.status(201).json({ message: "User created" });
   return;
