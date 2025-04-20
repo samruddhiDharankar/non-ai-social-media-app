d549f67e4fe98b45c4cad277a9205c218cfd1efa
samruddhiDharankar <samruddhee.dharankar@gmail.com>
2025-04-19 17:05:53 -0700

    added more tiers and their avg scores

--- backend/src/utils/tierUtils.ts
+++ backend/src/utils/tierUtils.ts
@@ -1,6 +1,11 @@
 export const getUserTier = (avgScore: number) => {
-  if (avgScore >= 85) return { tier: "The Real Deal", badge: "🔥" };
-  if (avgScore >= 70) return { tier: "Authentic Voice", badge: "🎤" };
-  if (avgScore >= 50) return { tier: "Believable", badge: "👌" };
-  return { tier: "Rookie", badge: "🍼" };
+  if (avgScore >= 95) return { tier: "Unfiltered Legend", badge: "🌌" };
+  if (avgScore >= 90) return { tier: "Icon", badge: "👑" };
+  if (avgScore >= 80) return { tier: "The Real Deal", badge: "🔥" };
+  if (avgScore >= 70) return { tier: "Authentic Voice", badge: "✨" };
+  if (avgScore >= 60) return { tier: "Believable", badge: "🧩" };
+  if (avgScore >= 50) return { tier: "Observer", badge: "🔍" };
+  if (avgScore >= 1) return { tier: "Rookie", badge: "🎭" };
+
+  return { tier: "Newbie", badge: "🐣" };
 };
