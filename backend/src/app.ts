6590645da4d6e9c15ede870f9200d8fb3df0c7e1
samruddhiDharankar <samruddhee.dharankar@gmail.com>
2025-04-28 20:01:32 -0700

    removed it

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -24,9 +24,6 @@ app.use(
   })
 ); // your frontend port
 
-// Handle preflight requests
-app.options("*", cors());
-
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
