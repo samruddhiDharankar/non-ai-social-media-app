25c65b407a5abd7f577b8a2afec8fc2f37a190f7
samruddhiDharankar <samruddhee.dharankar@gmail.com>
2025-05-09 19:54:43 -0700

    added rate limiting

--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -6,12 +6,13 @@ import {
   getPostByUserId,
 } from "../controllers/postController";
 import { protect } from "../middleware/authMiddleware";
+import { postRateLimiter } from "../middleware/rateLimitMiddleware";
 
 const router = Router();
 
 router.get("/feed", protect, getAllPosts);
 router.get("/user/:id", protect, getPostByUserId);
 router.get("/:id", protect, getPostById);
-router.post("/", protect, createPost);
+router.post("/", protect, postRateLimiter, createPost);
 
 export default router;
